580e3647e2832efbb6616c9fbb278dea62985b87
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-01-21 15:29:59 -0700

    created html template and created a user input window for game

---
+++ function.js
@@ -0,0 +1,11 @@
+// settings variables to keep score 
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// setting an array to give the computers options to choose 
+// INDEX      0    1     2
+var index = ['R', 'P', 'S',];
+
+//creating a prompt window to get user's input
+var userInput = window.prompt('Do you choose R, P, S ?')
