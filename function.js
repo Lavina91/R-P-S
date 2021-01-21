ca043c274a701d87d9d467939a93e996e7d34475
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-01-21 16:34:40 -0700

    created function to allow computer to chose a random value from choices variable and dispaly back to user

--- function.js
+++ function.js
@@ -5,29 +5,47 @@ var ties = 0;
 
 // setting an array to give the computers options to choose 
 // INDEX      0    1     2
-var index = ['R', 'P', 'S',];
+var choices = ['R', 'P', 'S'];
 
 
 // function expression that is the main controls of game 
-var letsPlay = function (){
+var letsPlay = function() {
 
  // creating a prompt window to get user's input
 var userInput = window.prompt('Do you choose R, P, S ?');
-//    if (userInput === 'R'){
-//     return 'You chose rock !';
-//   } 
-//    else if (userInput === 'P'){
-//      return 'You chose paper !';
-//   }
-//     else if (userInput === 'S'){
-//       return 'You chose scissor !';
-//   }
-//     else {
-//     return 'Please choose again !';
-//   }
+  
+// checks user's input and repeats it back 
+     if (!userInput) {
+         return;
+     }
  
+  //automatically puts the user input in upper case 
+userInput = userInput.toUpperCase
+
+// creating a variable that allows it to grab a random value from the array in var index. 
+var getIndex = Math.floor(Math.random()* choices.length);
+var  compChoice = choices[getIndex]; 
+
+window.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-// creating a variable that allows it to grab a random value 
-     // from the array in var index 
-var compInput = Math.floor(Math.random()* index.length);
+
+
 
