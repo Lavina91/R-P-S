cdbb3084ecce2312f734a722a75c87ada5f5069c
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-01-21 16:20:12 -0700

    added a if statement to check users input and repeat it back

--- function.js
+++ function.js
@@ -7,5 +7,27 @@ var ties = 0;
 // INDEX      0    1     2
 var index = ['R', 'P', 'S',];
 
-//creating a prompt window to get user's input
-var userInput = window.prompt('Do you choose R, P, S ?')
+
+// function expression that is the main controls of game 
+var letsPlay = function (){
+
+ // creating a prompt window to get user's input
+var userInput = window.prompt('Do you choose R, P, S ?');
+//    if (userInput === 'R'){
+//     return 'You chose rock !';
+//   } 
+//    else if (userInput === 'P'){
+//      return 'You chose paper !';
+//   }
+//     else if (userInput === 'S'){
+//       return 'You chose scissor !';
+//   }
+//     else {
+//     return 'Please choose again !';
+//   }
+ 
+}
+// creating a variable that allows it to grab a random value 
+     // from the array in var index 
+var compInput = Math.floor(Math.random()* index.length);
+
