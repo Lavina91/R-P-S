b4199a0d2626bd141e138b8cc80d1fc5acd0c66b
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-01-21 18:29:11 -0700

    Created an alert box to display what you chose

--- function.js
+++ function.js
@@ -15,35 +15,44 @@ var letsPlay = function() {
 var userInput = window.prompt('Do you choose R, P, S ?');
   
 // checks user's input and repeats it back 
-     if (!userInput) {
-         return 'You chose ' + userInput + ' !';
+     if (
+         (userInput === 'R') ||
+         (userInput === 'P') ||
+         (userInput === 'S') 
+         ) {
+         window.alert('You chose ' + userInput + ' !');
+     }
+     else {
+         window.alert('Wrong Answer !')
+         var userInput = window.prompt('Do you choose R, P, S ?');
+          
      }
  
   //automatically puts the user input in upper case 
-userInput = userInput.toUpperCase
+userInput = userInput.toUpperCase();
 
 // creating a variable that allows it to grab a random value from the array in var index. 
 var getIndex = Math.floor(Math.random()* choices.length);
 var  compChoice = choices[getIndex]; 
 
 // creates an alert window to display computers choice 
-window.alert('Computer chose ' + compChoice + ' !')
+window.alert('Computer chose ' + compChoice + ' !');
 
 
 // if statement to check if it is a tie
-if(userInput === compChoice){
-    ties++
-    window.alert('Its a tie.');
+if(userInput === compChoice) {
+    ties++;
+    window.alert('Its a tie !');
 }
 
 // else if statement to check if user won
 else if (
           (userInput === 'R' && compChoice ==='S') ||
-          (userInput === 'P' && compChoice === 'R') ||
+          (userInput === 'P' && compChoice === 'R')||
           (userInput === 'S' && compChoice === 'P')  
-                                                          ) {
-                     wins++;  
-                 window.alert("You've Won !");                                                   
+        ) {
+             wins++;  
+            window.alert("You've Won !");                                                   
     }
 
 // else statement to see if computer won 
@@ -55,18 +64,23 @@ else {
 
 // creates window to display stats
 window.alert(
-    'Stats:/nWins:' + wins + '/nLosses:' + losses + '/nTies:' + ties
+    'Stats:\nWins:' + wins + '\nLosses:' + losses + '\nTies:' + ties
 );
 
-var playAnotherRound = window.confirm('Do you want to play another round ?')
+//creates a window to ask the user if they want to play again 
+var playAnotherRound = window.confirm('Do you want to play another round ?');
+
+// stops if user chooses cancel
 if(! playAnotherRound){
     return;
 }
+// or they can play again 
 else {
-    playGame();
+    letsPlay();
   }
 }
 
-playGame()
+// calls function to run 1 time 
+letsPlay()
 
 
